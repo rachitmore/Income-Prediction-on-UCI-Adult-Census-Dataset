4e3b7e7f002a993e3de9459fc619d78c90be1b33
Rachit More <rachitmore3@gmail.com>
2024-08-09 20:48:48 +0530

    Security Configuration updated

--- Income_prediction/settings.py
+++ Income_prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k!3bo^a!=ydy(d-1!@jrfl+1x63&^kf%q-!(na34r6=h@#b!hp"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
